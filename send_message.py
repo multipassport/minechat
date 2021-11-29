a704c2ecadd8fff05c0648138405addc46652eb2
multipassport <mashross@gmail.com>
2021-11-30 01:35:59 +0300

    Use CLI argument to send a message

--- send_message.py
+++ send_message.py
@@ -10,22 +10,32 @@ logger = logging.getLogger('__name__')
 
 
 def parse():
-    parser = configargparse.ArgumentParser(default_config_files=['./config.txt'])
+    parser = configargparse.ArgumentParser(default_config_files=['./sender_config.txt'])
     parser.add('--host', help='Chat host')
     parser.add('--port', help='Chat port')
     parser.add('--history', help='Where to save chat history')
+    parser.add('message', help='Message to send')
     return parser
 
 
-async def send_message(parser_args, message):
+async def send_message(parser_args):
     reader, writer = await asyncio.open_connection(
-        parser_args.host, 5050
+        parser_args.host, parser_args.port,
     )
 
     account_hash = 'bc744a84-5161-11ec-8c47-0242ac110002'
-    message_to_send = f'{account_hash}\n{message}\n\n'
+    message_to_send = f'{account_hash}\n{parser_args.message}\n\n'
+    print(message_to_send)
+
+    server_reply = await reader.readline()
+    print(server_reply.decode())
+    logging.info(server_reply.decode())
 
     writer.write(message_to_send.encode())
+
+    server_reply_json = await reader.readline()
+    print(server_reply_json.decode())
+    logging.info(server_reply_json.decode())
 
     writer.close()
 
@@ -33,4 +43,10 @@ async def send_message(parser_args, message):
 if __name__ == '__main__':
     parser_args = parse().parse_args()
 
-    asyncio.run(send_message(parser_args, 'Message'))
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        filename='chatlog.log',
+    )
+
+    asyncio.run(send_message(parser_args))
