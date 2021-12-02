0fd8c18487752428af2155c406cb7801293b758c
multipassport <mashross@gmail.com>
2021-12-02 15:24:05 +0300

    Transfer arguments from variable instead of parser_args namespace

--- read_chat.py
+++ read_chat.py
@@ -9,7 +9,7 @@ import datetime
 logger = logging.getLogger(__name__)
 
 
-def parse():
+def parse_cli_args():
     parser = configargparse.ArgumentParser(default_config_files=['./reader_config.txt'])
     parser.add('--host', help='Chat host')
     parser.add('--port', help='Chat port')
@@ -17,11 +17,11 @@ def parse():
     return parser
 
 
-async def connect_to_chat(parser_args):
+async def connect_to_chat(chat_host, chat_port, history_location):
     logging.debug('Connecting to chat')
 
     reader, writer = await asyncio.open_connection(
-        parser_args.host, parser_args.port,
+        chat_host, chat_port,
     )
 
     while True:
@@ -30,18 +30,23 @@ async def connect_to_chat(parser_args):
         decoded_message = chat_message.decode()
         message_with_timestamp = f'[{message_time}] {decoded_message}'
 
-        async with aiofiles.open(parser_args.history, mode='a', encoding='utf-8') as file:
+        async with aiofiles.open(history_location, mode='a', encoding='utf-8') as file:
             await file.writelines(message_with_timestamp)
 
         print(message_with_timestamp)
 
 
 if __name__ == '__main__':
-    parser_args = parse().parse_args()
+    parser_args = parse_cli_args().parse_args()
+
+    chat_host = parser_args.host
+    chat_port = parser_args.port
+    history_location = parser_args.history
 
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO,
         filename='read_chat.log',
     )
-    asyncio.run(connect_to_chat(parser_args))
+
+    asyncio.run(connect_to_chat(chat_host, chat_port, history_location))
